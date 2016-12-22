0e435fac914a339d3c48120712bf99c0c4a06b4b
MYPC\Антон <toxoxa@gmail.com>
2016-12-22 20:56:51 +0300

    es6 classes

--- train/Train.js
+++ train/Train.js
@@ -1,20 +1,24 @@
-var Train = new function(data) {
-   this.number = data.number;
-   this.cost = data.cost;
-   this.startPoint = data.startPoint;
-   this.finishPoint = data.finishPoint;
-   this.startTime = data.startTime;
-   this.finishTime = data.finishPoint;
-}
+'use strict';
 
-Train.prototype.getData = function() {
-   return {
-      number: this.number,
-      cost: this.cost,
-      startPoint: this.startPoint,
-      finishPoint: this.finishPoint.
-      startTime: this.startTime,
-      finishTime : this.finishTime
+var Train = class {
+   constructor(data){
+      this.number = data.number;
+      this.cost = data.cost;
+      this.startPoint = data.startPoint;
+      this.finishPoint = data.finishPoint;
+      this.startTime = data.startTime;
+      this.finishTime = data.finishPoint;
+   }
+
+   getTrain() {
+      return {
+         number: this.number,
+         cost: this.cost,
+         startPoint: this.startPoint,
+         finishPoint: this.finishPoint.
+         startTime: this.startTime,
+         finishTime : this.finishTime
+      }
    }
 }
 
--- train/Trains.js
+++ train/Trains.js
@@ -1,32 +1,36 @@
+'use strict';
+
 var Train = require("./Train");
 
-var Trains = function(){
-   this.trains = [];
-};
+var Trains - class {
+   counstructor() {
+      this.trains = [];
+   };
 
-Trains.prototype.addTrain = function(data) {
-   this.trains.push(new Tour(data));
-};
+   addTrain(data) {
+      this.trains.push(new Tour(data));
+   };
 
-Trains.prototype.deleteTrain = function(trainNumber) {
-   if(this.trains[trainNumber])
-      this.trains.splice(trainNumber, 1);
-}
+   deleteTrain(trainNumber) {
+      if(this.trains[trainNumber])
+         this.trains.splice(trainNumber, 1);
+   }
 
-Trains.prototype.showStartTime = function(startPoint, finishTime) {
-   var result = [];
-   for(var i = 0; i < this.trains.length; i++)
-      if(this.trains[i].startPoint === startPoint && this.trains.finishTime === finishTime)
-         result.push(this.trains[i]);
-   return result;
-}
+   showStartTime(startPoint, finishTime) {
+      var result = [];
+      for(var i = 0; i < this.trains.length; i++)
+         if(this.trains[i].startPoint === startPoint && this.trains.finishTime === finishTime)
+            result.push(this.trains[i]);
+      return result;
+   }
 
-Trains.prototype.showStartFinish = function(startPoint, finishPoint) {
-   var result = [];
-   for(var i = 0; i < this.trains.length; i++)
-      if(this.trains[i].startPoint === startPoint && this.trains.finishPoint === finishPoint)
-         result.push(this.trains[i]);
-   return result;
+   showStartFinish(startPoint, finishPoint) {
+      var result = [];
+      for(var i = 0; i < this.trains.length; i++)
+         if(this.trains[i].startPoint === startPoint && this.trains.finishPoint === finishPoint)
+            result.push(this.trains[i]);
+      return result;
+   }
 }
 
 module.exports = Tours;
